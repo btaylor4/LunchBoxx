a0280501f1422cf89a639c35f46ac5faf914d2a2
Evan Ristow <evanri@a12700evanri.us.corp>
2018-06-20 10:25:11 -0400

    fixing up user-portal

--- app/server/server.py
+++ app/server/server.py
@@ -32,162 +32,162 @@ login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(email):
-    user = mongo.db.users.find_one({"email": email})
-    if not user:
-        return None
-    new_user = User(email)
-    new_user.db_user(user)
-    return new_user
+	user = mongo.db.users.find_one({"email": email})
+	if not user:
+		return None
+	new_user = User(email)
+	new_user.db_user(user)
+	return new_user
 
 def getInterests():
-    with open('interests.json') as f:
-        data = json.load(f)
-        return data['interests']
+	with open('interests.json') as f:
+		data = json.load(f)
+		return data['interests']
 
 def getFoods():
-    with open('foods.json') as f:
-        data = json.load(f)
-        return data['foods']
+	with open('foods.json') as f:
+		data = json.load(f)
+		return data['foods']
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
+	if request.method == 'POST':
 
-        # routing from signup
-        if(request.form['nextButton'] == 'create-profile'):
-            email = request.form['email']
-            password = request.form['password']
-            verify_password = request.form['verify-password']
+		# routing from signup
+		if(request.form['nextButton'] == 'create-profile'):
+			email = request.form['email']
+			password = request.form['password']
+			verify_password = request.form['verify-password']
 
-            hashed_password = generate_password_hash(password, method='sha256')
-            # searches the data base for the username chosen
-            requested_user = mongo.db.users.find_one({'email': email})
+			hashed_password = generate_password_hash(password, method='sha256')
+			# searches the data base for the username chosen
+			requested_user = mongo.db.users.find_one({'email': email})
 
-            print('daddy data: ', getInterests())
+			print('daddy data: ', getInterests())
 
-            if requested_user is None:
-                if password == verify_password:
-                    if(len(password) > 5):
-                        # makes a new user inside data base if non already exits
-                        mongo.db.users.insert(
-                            {'email': email, 'password': hashed_password})
-                        return render_template('create-profile.html', email=email, password=password, hidden='hidden', interests=getInterests(), foods=getFoods())
-                    else:
-                        return render_template('sign-up.html', error='Passwords must be at least 6 characters.')
-                else:
-                    return render_template('sign-up.html', error='Passwords do not match.')
-            else:
-                return render_template('sign-up.html', error='Username has already been taken.')
+			if requested_user is None:
+				if password == verify_password:
+					if(len(password) > 5):
+						# makes a new user inside data base if non already exits
+						mongo.db.users.insert(
+							{'email': email, 'password': hashed_password})
+						return render_template('create-profile.html', email=email, password=password, hidden='hidden', interests=getInterests(), foods=getFoods())
+					else:
+						return render_template('sign-up.html', error='Passwords must be at least 6 characters.')
+				else:
+					return render_template('sign-up.html', error='Passwords do not match.')
+			else:
+				return render_template('sign-up.html', error='Username has already been taken.')
 
-        # routing from create profile page
-        elif(request.form['nextButton'] == 'done'):
-            # create the user
-            form = request.form
+		# routing from create profile page
+		elif(request.form['nextButton'] == 'done'):
+			# create the user
+			form = request.form
 
-            email = form['email']
-            password = form['password']
+			email = form['email']
+			password = form['password']
 
-            user = User(email)
+			user = User(email)
 
-            print('form[\'lunch-time\']:', form['lunch-time'])
+			print('form[\'lunch-time\']:', form['lunch-time'])
 
-            now = datetime.utcnow()
+			now = datetime.utcnow()
 
-            print('now:', now)
+			print('now:', now)
 
-            tempTimeString = now.strftime("%d%m%Y") + " " + form['lunch-time']
+			tempTimeString = now.strftime("%d%m%Y") + " " + form['lunch-time']
 
-            print('tempTimeString:', tempTimeString)
+			print('tempTimeString:', tempTimeString)
 
-            tempTime = datetime.strptime(
-                tempTimeString, "%d%m%Y %I:%M %p")
+			tempTime = datetime.strptime(
+				tempTimeString, "%d%m%Y %I:%M %p")
 
-            print('tempTime:', tempTime)
+			print('tempTime:', tempTime)
 
-            timeDiff = (tempTime-datetime(1970, 1, 1)).total_seconds()
+			timeDiff = (tempTime-datetime(1970, 1, 1)).total_seconds()
 
-            print('timeDiff:', timeDiff)
+			print('timeDiff:', timeDiff)
 
-            user.first_name = form['firstName']
-            user.last_name = form['lastName']
-            user.time_pref = round(timeDiff)
-            user.addr = form['address']
+			user.first_name = form['firstName']
+			user.last_name = form['lastName']
+			user.time_pref = round(timeDiff)
+			user.addr = form['address']
 
-            # handle errors
-            if(user.first_name == ''):
-                return render_template('create-profile.html', email=email, password=password, error='First name can not be empty.', interests=getInterests(), foods=getFoods())
-            elif(user.last_name == ''):
-                return render_template('create-profile.html', email=email, password=password, error='Last name can not be empty.', interests=getInterests(), foods=getFoods())
-            elif(user.addr == ''):  # TODO: see if address exits (google maps)
-                return render_template('create-profile.html', email=email, password=password, error='Address is invalid.', interests=getInterests(), foods=getFoods())
+			# handle errors
+			if(user.first_name == ''):
+				return render_template('create-profile.html', email=email, password=password, error='First name can not be empty.', interests=getInterests(), foods=getFoods())
+			elif(user.last_name == ''):
+				return render_template('create-profile.html', email=email, password=password, error='Last name can not be empty.', interests=getInterests(), foods=getFoods())
+			elif(user.addr == ''):  # TODO: see if address exits (google maps)
+				return render_template('create-profile.html', email=email, password=password, error='Address is invalid.', interests=getInterests(), foods=getFoods())
 
-            # preferences
-            if('interests' in form):
-                user.interest_prefs = form.getlist('interests')
-            if('food' in form):
-                user.food_prefs = form.getlist('food')
-            update = {'first_name': user.first_name, 'last_name': user.last_name, 'time_pref': user.time_pref, 'addr': user.addr,
-                      'interest_prefs': user.interest_prefs, 'food_prefs': user.food_prefs, 'status': "not_matched"}
+			# preferences
+			if('interests' in form):
+				user.interest_prefs = form.getlist('interests')
+			if('food' in form):
+				user.food_prefs = form.getlist('food')
+			update = {'first_name': user.first_name, 'last_name': user.last_name, 'time_pref': user.time_pref, 'addr': user.addr,
+					  'interest_prefs': user.interest_prefs, 'food_prefs': user.food_prefs, 'status': "not_matched"}
 
-            # find and update user
-            mongo.db.users.update_one({'email': user.email}, {'$set': update})
+			# find and update user
+			mongo.db.users.update_one({'email': user.email}, {'$set': update})
 
 
-            # redirect to the login page
-            return redirect(url_for('login'))
-        elif(request.form['nextButton'] == 'back'):
-            return redirect(url_for('login'))
+			# redirect to the login page
+			return redirect(url_for('login'))
+		elif(request.form['nextButton'] == 'back'):
+			return redirect(url_for('login'))
 
-    return render_template('sign-up.html', hidden='hidden')
+	return render_template('sign-up.html', hidden='hidden')
 
 
 # sets up the page for registration
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
+	if request.method == 'POST':
+		email = request.form['email']
+		password = request.form['password']
 
-        requested_user = mongo.db.users.find_one({'email': email})
-        if requested_user:
-            if check_password_hash(requested_user["password"], password):
-                user = User(email=request.form['email'])
-                login_user(user)
-                return redirect(url_for('user_portal'))
-        return render_template('login.html', error='Incorrect username or password.')
+		requested_user = mongo.db.users.find_one({'email': email})
+		if requested_user:
+			if check_password_hash(requested_user["password"], password):
+				user = User(email=request.form['email'])
+				login_user(user)
+				return redirect(url_for('user_portal'))
+		return render_template('login.html', error='Incorrect username or password.')
 
-    return render_template('login.html', hidden='hidden')
+	return render_template('login.html', hidden='hidden')
 
 
 @app.route('/preferences', methods=['GET', 'POST'])
 @login_required
 def preferences():
-    if request.method == 'POST':
+	if request.method == 'POST':
 
-        now = datetime.utcnow()
-        tempTimeString = now.strftime("%d%m%Y") + " " + request.form['lunch-time']
-        tempTime = datetime.strptime(tempTimeString, "%d%m%Y %I:%M %p")
-        timeDiff = (tempTime-datetime(1970,1,1)).total_seconds()
+		now = datetime.utcnow()
+		tempTimeString = now.strftime("%d%m%Y") + " " + request.form['lunch-time']
+		tempTime = datetime.strptime(tempTimeString, "%d%m%Y %I:%M %p")
+		timeDiff = (tempTime-datetime(1970,1,1)).total_seconds()
 
 
-        mongo.db.being_matched.insert({'email': current_user.email, 'preferences': request.form.getlist('food'), 'time_pref': timeDiff })
-        mongo.db.users.update({'email': current_user.email}, {'$set': {'status': "being_matched"}})
-        value = form_groups(mongo.db.users, mongo.db.being_matched, mongo.db.groups)
-        print(value)
-        if(value):
-            # Return to bryan's page
-            return "You were matched"
-            pass
-        else:
-            return redirect(url_for('matching', time_pref=round(timeDiff)))
+		mongo.db.being_matched.insert({'email': current_user.email, 'preferences': request.form.getlist('food'), 'time_pref': timeDiff })
+		mongo.db.users.update({'email': current_user.email}, {'$set': {'status': "being_matched"}})
+		value = form_groups(mongo.db.users, mongo.db.being_matched, mongo.db.groups)
+		print(value)
+		if(value):
+			# Return to bryan's page
+			return "You were matched"
+			pass
+		else:
+			return redirect(url_for('matching', time_pref=round(timeDiff)))
 
-    time = datetime.utcfromtimestamp(current_user.time_pref)
-    time_string = time.strftime("%I:%M %p")
-    time_string2 = "'" + time_string + "'"
-    print("time_string", time_string)
-    print("time_string2", time_string2)
-    return render_template('preferences.html', preference_list=current_user.food_prefs, time_pref=time_string2, foods=getFoods())
+	time = datetime.utcfromtimestamp(current_user.time_pref)
+	time_string = time.strftime("%I:%M %p")
+	time_string2 = "'" + time_string + "'"
+	print("time_string", time_string)
+	print("time_string2", time_string2)
+	return render_template('preferences.html', preference_list=current_user.food_prefs, time_pref=time_string2, foods=getFoods())
 
 
 @app.route('/places', methods=['GET', 'POST'])
@@ -205,13 +205,13 @@ def place():
 
 @app.route("/logout")
 def logout():
-    logout_user()
-    return redirect(url_for('login'))
+	logout_user()
+	return redirect(url_for('login'))
 
 
 @app.route("/")
 def index():  # TODO: Check if user is logged in
-    return redirect(url_for("login"))
+	return redirect(url_for("login"))
 
 
 @app.route("/user-portal")
@@ -232,22 +232,24 @@ def getUserDict():
 		u[propertyName] = property
 
 	del u['Password']
+	del u['Status']
 
-	# u['Time Pref'] = datetime.strptime(u['Time Pref'])
+	time = datetime.utcfromtimestamp(u['Time Pref'])
+	u['Time Pref'] = time.strftime("%I:%M %p")
 
 	return u
 
 @app.route("/matching")
 def matching():
-    print('current_user.time_pref:', current_user.time_pref)
-    print('time_pref:', request.args['time_pref'])
-    # produce datetime obj from seconds-since-epoch time_pref on current_user (add subtract 30 minutes to get notification time)
-    dateFormatted = datetime.utcfromtimestamp(
-        float(request.args['time_pref']) - 1800.0)
+	print('current_user.time_pref:', current_user.time_pref)
+	print('time_pref:', request.args['time_pref'])
+	# produce datetime obj from seconds-since-epoch time_pref on current_user (add subtract 30 minutes to get notification time)
+	dateFormatted = datetime.utcfromtimestamp(
+		float(request.args['time_pref']) - 1800.0)
 
-    # send current_user's email and formatted time to matching.html
-    return render_template("matching.html", time=(dateFormatted.strftime('%I:%M %p')))
+	# send current_user's email and formatted time to matching.html
+	return render_template("matching.html", time=(dateFormatted.strftime('%I:%M %p')))
 
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)  # debug = true to put in debug mod
+	socketio.run(app, debug=True)  # debug = true to put in debug mod
